f8c809f38557593a75f2cd2efc47def2ca16047e
Deumek <53879371+Deumek@users.noreply.github.com>
2021-12-12 12:53:54 -0500

    Creación clases Asignatura y clase
    Creación de los constructores y métodos set y get

--- src/gestorAplicacion/horario/Asignatura.java
+++ src/gestorAplicacion/horario/Asignatura.java
@@ -1,5 +1,38 @@
 package gestorAplicacion.horario;
+import java.util.ArrayList;
 
 public class Asignatura {
+    private String profesor;
+    private String nombre;
+    private ArrayList<Clase> clases;
 
+    Asignatura(String profesor, String nombre, ArrayList<Clase> clases) {
+        this.profesor = profesor;
+        this.nombre = nombre;
+        this.clases = clases;
+    }
+
+    public String getProfesor() {
+        return profesor;
+    }
+
+    public void setProfesor(String profesor) {
+        this.profesor = profesor;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public ArrayList<Clase> getClases() {
+        return clases;
+    }
+
+    public void addClase(Clase clase) {//Agrega un objeto clase al arrayList clases
+        clases.add(clase);
+    }
 }
--- src/gestorAplicacion/horario/Clase.java
+++ src/gestorAplicacion/horario/Clase.java
@@ -1,5 +1,37 @@
 package gestorAplicacion.horario;
 
 public class Clase {
+    private int horaInicio;
+    private int horaFin;
+    private String dia;
 
+    public Clase(int horaInicio, int horaFin, String dia) {
+        this.horaInicio = horaInicio;
+        this.horaFin = horaFin;
+        this.dia = dia;
+    }
+
+    public void setHoraInicio(int inicio) {
+        horaInicio = inicio;
+    }
+
+    public int getHoraInicio() {
+        return horaInicio;
+    }
+
+    public void setHoraFin(int fin) {
+        horaInicio = fin;
+    }
+
+    public int getHoraFin() {
+        return horaFin;
+    }
+
+    public void setDia(String dia) {
+        this.dia = dia;
+    }
+
+    public String getDia() {
+        return dia;
+    }
 }
