5fe7756058c4975c4f4fc776d4d2df7e208f44cf
Deumek <53879371+Deumek@users.noreply.github.com>
2021-12-14 21:01:53 -0500

    Agregar métodos get y set para la clase tarea

--- src/gestorAplicacion/tareas/Tarea.java
+++ src/gestorAplicacion/tareas/Tarea.java
@@ -7,7 +7,38 @@ public abstract class Tarea {
 	 String titulo;
 	 Asignatura asignatura;
 	 String fechaEntrega;
-	 Estado estado;
+	 
+	 public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public Asignatura getAsignatura() {
+		return asignatura;
+	}
+	public void setAsignatura(Asignatura asignatura) {
+		this.asignatura = asignatura;
+	}
+	public String getFechaEntrega() {
+		return fechaEntrega;
+	}
+	public void setFechaEntrega(String fechaEntrega) {
+		this.fechaEntrega = fechaEntrega;
+	}
+	public Estado getEstado() {
+		return estado;
+	}
+	public void setEstado(Estado estado) {
+		this.estado = estado;
+	}
+	public ArrayList<Nota> getNotas() {
+		return notas;
+	}
+	public void setNotas(ArrayList<Nota> notas) {
+		this.notas = notas;
+	}
+	Estado estado;
 	 ArrayList<Nota> notas;
 	 
 	 
