f2cbdb9dc1406fc8de1e5e6d96866b16a78c0683
SebastianGaviriaM <sebastiangaviriamontoya@gmail.com>
2022-02-01 14:39:50 -0500

    Interfaz 2/4

--- python/interfaz/menuPrincipal.py
+++ python/interfaz/menuPrincipal.py
@@ -3,16 +3,19 @@ from tkinter import *
 from ventana import Ventana
 
 class MenuPrincipal(Menu):
-    def __init__(self, window):
+    def __init__(self, window, *args):
         super().__init__(window)
-
+        menu1 = Menu(self)
         
         def cerrarApp():
-            window.destroy()
+            if(args[0]=="Inicio"):
+                window.destroy()
+
+            
 
 
+        if(args[0]=="Inicio"):    
+            self.add_cascade(menu = menu1, label="Opciones")
+            menu1.add_command(label = "Descripcion")
+            menu1.add_command(label = "Salir", command=cerrarApp)
 
-        menu1 = Menu(self)
-        self.add_cascade(menu = menu1, label="Opciones")
-        menu1.add_command(label = "Descripcion")
-        menu1.add_command(label = "Salir", command=cerrarApp)
--- python/interfaz/ventana.py
+++ python/interfaz/ventana.py
@@ -11,8 +11,12 @@ class Ventana(Frame):
         elif('width' in kwargs):
             super().__init__(window, width=kwargs['width'], height=kwargs['height'])
             self.config(bg=kwargs['bg'])
+    
+    def cerrarVentana(self):
+        self.grid_forget()
 
-        
+    def mostrarVentana(self):
+        self.grid(row=1, column=1)
 
         
 
