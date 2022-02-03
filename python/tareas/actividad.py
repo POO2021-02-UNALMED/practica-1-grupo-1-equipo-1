4e9e3861be6585e60416674e0c07caf1a4677e47
Deumek <53879371+Deumek@users.noreply.github.com>
2022-02-03 15:55:26 -0500

    cambios a actividad y proyecto sobre el error
    si se va a cambiar, se puede deshacer este cambio únicamente, para no generar errores

--- python/tareas/actividad.py
+++ python/tareas/actividad.py
@@ -3,9 +3,9 @@ from tareas.tarea import tarea
 
 #Objetos de tipo tarea y proyecto pero que a diferencia de proyecto solo admite objetos que solo sean tipo tarea
 class actividad(proyecto, tarea):
-    def __init__(self, titulo, asignatura, fechaDeEntrega, estado):
+    def __init__(self, titulo, asignatura, fechaDeEntrega):
         self.descripcion
-        super().__init__(titulo, asignatura, fechaDeEntrega, estado)
+        super().__init__(titulo, asignatura, fechaDeEntrega)
     
     
 
--- python/tareas/proyecto.py
+++ python/tareas/proyecto.py
@@ -1,18 +1,18 @@
-
 from tareas.tarea import tarea
-from tareas.actividad import actividad
+#from tareas.actividad import *
 from tareas.estado import estado
+from revErrores.revErrores import revErrores
 
 #objetos tarea que puede ser dividido en otras tareas
-class proyecto(tarea): 
+class proyecto(tarea):
     def __init__(self, titulo, asignatura, fechaDeEntrega):
         self.actividades =[]
         super().__init__(titulo, asignatura, fechaDeEntrega)
 
     def dividirTarea(self, num, *args): #esta funcion divide un proyecto en una cantidad de tareas deseadas
         for i in range(0, num):
-
-            fragmentoDeTarea = actividad("Actividad "+(i+1)+"de "+self.titulo, self.asignatura, args[i], estado.sinCompletar.value)
+            revErrores.compararFechas(args[i], self.fechaDeEntrega)
+            fragmentoDeTarea = actividad("Actividad "+str(i+1)+"de "+self.titulo, self.asignatura, args[i])
             self.actividades.append(fragmentoDeTarea)
     
 
@@ -24,3 +24,8 @@ class proyecto(tarea):
 
         if(completada):
             self.estado = estado.completada.value
+
+class actividad(proyecto, tarea):
+    def __init__(self, titulo, asignatura, fechaDeEntrega):
+        #self.descripcion
+        super().__init__(titulo, asignatura, fechaDeEntrega)
